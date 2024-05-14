16ca267b19ccf8c50f7e7d75ceccf7a0ed43ca1a
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-05-14 12:49:49 +0530

    created game window

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,30 @@
+#include<SFML/Graphics.hpp>
+using namespace sf;
 
 int main()
 {
+    //defining dimensions of video mode
+    VideoMode videoMode = *(new VideoMode(800, 600));
+
+    //rendering window of specified size
+    RenderWindow* window = new RenderWindow(videoMode, "My SFML Window!");
+
+    //Game loop to keep window open
+    while(window->isOpen()) 
+    {
+        Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == Event::Closed)    //checking if window closed
+            {
+                window->close();
+            }
+        }
+
+        window->clear(Color::Red); //clearing the window with color blue
+
+        window->display();      //displaying objects on the window
+    }
+
     return 0;
 }

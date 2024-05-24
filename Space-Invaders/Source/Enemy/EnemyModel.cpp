aea66a05852697535b17629fdee69371c994a02c
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-05-24 12:24:51 +0530

    implemented enemy movement

--- Space-Invaders/Source/Enemy/EnemyModel.cpp
+++ Space-Invaders/Source/Enemy/EnemyModel.cpp
@@ -14,6 +14,7 @@ namespace Enemy
 
 	void EnemyModel::initialize()
 	{
+		movement_direction = MovementDirection::RIGHT;
 		enemy_position = reference_position;
 	}
 
@@ -45,5 +46,15 @@ namespace Enemy
 	void EnemyModel::setReferencePosition(sf::Vector2f position)
 	{
 		reference_position = position;
+	}
+
+	MovementDirection EnemyModel::getMovementDirection()
+	{
+		return movement_direction;
+	}
+
+	void EnemyModel::setMovementDirection(MovementDirection direction)
+	{
+		movement_direction = direction;
 	}
 }

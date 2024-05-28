a254865f28903e262c25cd829921a40144efa53a
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-05-28 13:03:20 +0530

    unique enemy functionality

--- Space-Invaders/Source/Enemy/Controllers/SubZeroController.cpp
+++ Space-Invaders/Source/Enemy/Controllers/SubZeroController.cpp
@@ -1,1 +1,32 @@
 #include "Enemy/Controllers/SubZeroController.h"
+
+namespace Enemy
+{
+	namespace Controller
+	{
+		SubZeroController::SubZeroController()
+		{
+
+		}
+
+		SubZeroController::~SubZeroController()
+		{
+
+		}
+
+		void SubZeroController::initialize()
+		{
+			EnemyController::initialize();
+		}
+
+		void SubZeroController::move()
+		{
+
+		}
+
+		void SubZeroController::moveDown()
+		{
+
+		}
+	}
+}

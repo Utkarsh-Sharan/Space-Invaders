bd166248813be583fb225548538fe9e614629502
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-06-03 15:09:41 +0530

    created sound service

---
+++ Space-Invaders/Source/Sound/SoundService.cpp
@@ -0,0 +1,29 @@
+#include "Sound/SoundService.h"
+
+namespace Sound
+{
+	void SoundService::loadBackgroundMusicFromFile()
+	{
+
+	}
+
+	void SoundService::loadSoundFromFile()
+	{
+
+	}
+
+	void SoundService::initialize()
+	{
+
+	}
+
+	void SoundService::playSound(SoundType type)
+	{
+
+	}
+
+	void SoundService::playBackgroundMusic()
+	{
+
+	}
+}

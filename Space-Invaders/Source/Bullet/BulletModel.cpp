f4cb763ba62817f27e6ce61093b521d1fc37aeeb
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-06-04 11:47:06 +0530

    created model and view for bullet

---
+++ Space-Invaders/Source/Bullet/BulletModel.cpp
@@ -0,0 +1,60 @@
+#include "Bullet/BulletModel.h"
+
+namespace Bullet
+{
+	BulletModel::BulletModel(Bullet::BulletType type)
+	{
+		bullet_type = type;
+	}
+
+	BulletModel::~BulletModel()
+	{
+
+	}
+
+	void BulletModel::initialize(sf::Vector2f position, Bullet::MovementDirection direction)
+	{
+		bullet_position = position;
+		movement_direction = direction;
+	}
+
+	sf::Vector2f BulletModel::getBulletPosition()
+	{
+		return bullet_position;
+	}
+
+	void BulletModel::setBulletPosition(sf::Vector2f position)
+	{
+		bullet_position = position;
+	}
+
+	BulletType BulletModel::getBulletType()
+	{
+		return bullet_type;
+	}
+
+	void BulletModel::setBulletType(BulletType type)
+	{
+		bullet_type = type;
+	}
+
+	MovementDirection BulletModel::getMovementDirection()
+	{
+		return movement_direction;
+	}
+
+	void BulletModel::setMovementDirection(MovementDirection direction)
+	{
+		movement_direction = direction;
+	}
+
+	float BulletModel::getMovementSpeed()
+	{
+		return movement_speed;
+	}
+
+	void BulletModel::setMovementSpeed(float speed)
+	{
+		movement_speed = speed;
+	}
+}
